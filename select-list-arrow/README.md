4f91c7b42a0ddbe849a391736e394b372f5196c4
Kathy Beck <kbeck@oomphinc.com>
2015-05-29 12:46:44 -0400

    Added a Select List css arrow and removed default firefox and internet explore arrow

---
+++ select-list-arrow/README.md
@@ -0,0 +1,5 @@
+# Remove browser default select list arrow and replace them with a consistant css arrow
+
+Added a <span class="css-arrow"> around the select list and added the arrow using borders on the span:after
+
+Markup may need to be modified in order to create the expected markup pattern.
---
+++ select-list-arrow/select-list-arrow.php
@@ -0,0 +1,12 @@
+/**
+ * This markup is a simple select list
+ */
+
+<span class="css-arrow">
+	<select name="neighborhood-filter">
+		<option value="">All Results</option>
+		<option value="value1">Value 1</option>
+		<option value="value2">Value 2</option>
+		<option value="value3">Value 3</option>
+	</select>
+</span>
